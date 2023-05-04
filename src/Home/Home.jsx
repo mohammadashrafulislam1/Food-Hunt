5c5e5b991129e839019b91f54e0e19aaac890b27
mohammadashrafulislam <ixdell41@gmail.com>
2023-05-04 15:47:24 +0600

    added another section

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import Chef from './Chef';
 import SectionTwo from './SectionTwo';
+import Contact from '../Contact/Contact';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Banner></Banner>
         <Chef></Chef>
         <SectionTwo></SectionTwo>
+        <Contact></Contact>
         </div>
     );
 };
--- src/shared/Navigation.jsx
+++ src/shared/Navigation.jsx
@@ -31,7 +31,7 @@ const Navigation = () => {
   <div className="dropdown dropdown-end">
   <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
     <div className="w-10 rounded-full">
-      <img src={user?.photoURL} />
+      <img src={user? user.photoURL :'https://cdn-icons-png.flaticon.com/512/2636/2636603.png'} />
     </div>
   </label>
   <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
